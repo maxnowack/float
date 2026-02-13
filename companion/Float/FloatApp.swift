dcf5997aabe3050388b9e12205b81ceaabb81e94
Max Nowack <maxnowack@users.noreply.github.com>
2026-02-13 11:27:43 +0100

    feat: add empty companion app

---
+++ companion/Float/FloatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatApp.swift
+//  Float
+//
+//  Created by Max Nowack on 13.02.26.
+//
+
+import SwiftUI
+
+@main
+struct FloatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
